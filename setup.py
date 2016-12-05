2d42e7acac239a313854093103a34a8ac2126add
zgs225 <zgs225@gmail.com>
2016-12-06 00:06:41 +0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,6 @@ setup(
     classifiers=(
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
-        'Natural Language :: Chinese',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
